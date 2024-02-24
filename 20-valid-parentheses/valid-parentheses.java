0d59b8eedb1a17455e0e7596d89b438093c9ad59
Sarthak Tyagi <50807462+sarthak98tyagi@users.noreply.github.com>
2024-02-24 22:27:33 +0530

    Time: 1 ms (98.77%) | Memory: 41.3 MB (68.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(st.isEmpty())
+                st.push(ch);
+            else{
+                if(ch==']' && st.peek()=='[')
+                    st.pop();
+                else if(ch==')' && st.peek()=='(')
+                    st.pop();
+                else if(ch=='}' && st.peek()=='{')
+                    st.pop();
+                else
+                    st.push(ch);
+            }
+        }
+        return st.isEmpty();
+    }
+}
