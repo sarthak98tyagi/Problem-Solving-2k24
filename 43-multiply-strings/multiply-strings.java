143e7d94e69b8b339880cdf504254d52fb67f730
Sarthak Tyagi <50807462+sarthak98tyagi@users.noreply.github.com>
2024-02-29 23:24:19 +0530

    Time: 3 ms (64.88%) | Memory: 42.7 MB (32.18%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String multiply(String s1, String s2) {
+        if(s1.equals("0") || s2.equals("0"))
+            return "0";
+        int n1 = s1.length(), n2 = s2.length();
+        if(n2>n1)
+            return multiply(s2,s1);
+        int[] ans = new int[n1+n2];
+        int an=-1;
+        for(int j=(n2-1);j>=0;j--){
+            int x = n2-j-1, c=0;
+            for(int i=(n1-1);i>=0;i--){
+                int pr = (s1.charAt(i)-'0')*(s2.charAt(j)-'0')+ans[x]+c;
+                ans[x] = pr % 10;
+                c = pr/10;
+                x++;
+                an=x;
+            }
+            if(c>0){
+                ans[x] = c;
+                x++;
+                an=x;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=(an-1);i>=0;i--){
+            sb.append((char)(ans[i]+'0')+"");
+        }
+        return sb.toString();
+
+                                                                   
+    }
+}
