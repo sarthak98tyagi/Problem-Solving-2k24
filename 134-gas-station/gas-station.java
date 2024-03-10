2c6b1cf493e581982c7d7077543c65c32430e631
Sarthak Tyagi <50807462+sarthak98tyagi@users.noreply.github.com>
2024-03-10 12:32:58 +0530

    Time: 1058 ms (5.00%) | Memory: 63.6 MB (5.18%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,8 +1,14 @@
+//-2
+//-4
+//-6
+//-3
+//0
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int cg=0,rg=0,i=0;
         int start=-1;
         while(i<gas.length){
+            System.out.println(i+" "+cg+" "+rg);
             if((gas[i]+cg)>=cost[i]){
                 cg = gas[i]+cg-cost[i];
                 if(start==-1)
