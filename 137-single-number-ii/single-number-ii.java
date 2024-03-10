e589e9461c19362d1d51fc812e7bd1735ffe7c3d
Sarthak Tyagi <50807462+sarthak98tyagi@users.noreply.github.com>
2024-03-10 14:26:26 +0530

    Time: 3 ms (67.04%) | Memory: 45 MB (88.27%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int el=0;
+        for(int i=0;i<32;i++){
+            int t = 0;
+            for(int x: arr){
+                if(((x>>i)&1)==1)
+                    t++;
+            }
+            if(t%3!=0)
+                t=t%3;
+            else
+                continue;
+            el=el|(1<<i);
+        }
+        return el;
+    }
+}
